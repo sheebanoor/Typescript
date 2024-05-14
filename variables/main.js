0d04f72f578a9a21400d2ce90994cf55f59935ad
sheeba noor <sheebanoor130@gmail.com>
2024-05-14 10:15:51 +0500

    Add files via upload

---
+++ variables/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// let myName="ali";
+// console.log(myName);
+// var myname="ali";
+// console.log(myname);
+// const myName="ali"
+// console.log(myName);
+// let myname:string="ali";
+// console.log(myname);
+// let num1:number=123456;
+// console.log
+// let value:boolean=true
+// console.log(value);
--- variables/main.ts
+++ variables/main.ts
@@ -1,1 +1,17 @@
+// let myName="ali";
+// console.log(myName);
+
+// var myname="ali";
+// console.log(myname);
+
+// const myName="ali"
+// console.log(myName);
+
+// let myname:string="ali";
+// console.log(myname);
+
+// let num1:number=123456;
+// console.log
+// let value:boolean=true
+// console.log(value);
 
