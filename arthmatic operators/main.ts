0977b36e16a05b2d533a7297efbb48390f4b79be
sheeba noor <sheebanoor130@gmail.com>
2024-05-14 12:03:55 +0500

    Add files via upload

--- arthmatic operators/main.ts
+++ arthmatic operators/main.ts
@@ -1,1 +1,25 @@
+// arthmatic operators
+//addition
 
+let num1=1
+let num2=5
+console.log(num1+num2);
+
+// // subtract
+ let value1=10
+ let value2=5
+console.log(value1-value2);
+
+// multiplication
+let n1=5
+let n2=5
+console.log(n1*n2);
+
+// division
+let sum1= 4
+let sum2 =2
+console.log(sum1/sum2);
+// exponentation
+let q1=2
+let q2=2
+console.log(q1**q2);
