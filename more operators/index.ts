e432456e7bc17226044d23aeb8e3b55d8e863a49
sheeba noor <sheebanoor130@gmail.com>
2024-05-14 14:13:36 +0500

    Add files via upload

---
+++ more operators/index.ts
@@ -0,0 +1,14 @@
+//assignment operator
+let num1=5
+console.log(num1+=5);
+
+//compaarison operators
+let x=10
+//console.log(x<10);
+console.log(x! ==10);
+
+// logical operator
+let ox =5
+console.log(ox = 5 || ox> 5);
+
+
