b5926109951021ee1e0e035820874ce42ca1becd
sheeba noor <sheebanoor130@gmail.com>
2024-05-16 12:52:14 +0500

    Add files via upload

---
+++ if else/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//if else
+// if(true){
+//     console.log("welcome home")
+// }
+//else{
+// console.log("bye")
--- if else/main.ts
+++ if else/main.ts
@@ -1,1 +1,6 @@
-
+//if else
+// if(true){
+//     console.log("welcome home")
+// }
+//else{
+    // console.log("bye")
