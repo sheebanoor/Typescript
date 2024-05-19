952ba6df300c140e148969cf7e157aac0b18a7d7
sheeba noor <sheebanoor130@gmail.com>
2024-05-19 08:20:57 +0500

    Delete if else&else if directory

--- if else&else if/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let username = "ali";
-let pw = "123456";
-if (username == "ali" && pw == "123456") {
-    console.log("sign in ");
-}
-else {
-    console.log("wrong password");
-}
--- if else&else if/index.ts
+++
@@ -1,8 +0,0 @@
-let username:string= "ali";
-let pw:string= "123456";
-if(username == "ali" && pw == "123456"){
-    console.log ("sign in ")
-}
-else {
-    console.log("wrong password")
-}
