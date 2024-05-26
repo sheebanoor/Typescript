80adeb46163a928ce49ffe718bab80292ac07c8a
sheeba noor <sheebanoor130@gmail.com>
2024-05-26 17:19:53 +0500

    Add files via upload

---
+++ function/index.js
@@ -0,0 +1,15 @@
+// function mysum (){
+//     console.log(10+10)
+// }
+// mysum()
+// function plus(){
+// let a=10
+// let b =20
+// console.log(a+b)
+// }
+// plus()
+//argument , perameter
+function num1(a) {
+    console.log(a);
+}
+num1(456);
--- function/index.ts
+++ function/index.ts
@@ -1,1 +1,20 @@
+// function mysum (){
+//     console.log(10+10)
+// }
+// mysum()
+
+// function plus(){
+// let a=10
+// let b =20
+// console.log(a+b)
+// }
+
+// plus()
+
+
+//argument , perameter
+function num1(a:number){
+    console.log(a)
+}
+num1(456)
 
