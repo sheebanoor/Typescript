e2ccd32b5f6cc3e0ae7a62f3002f9b98c35b2fab
sheeba noor <sheebanoor130@gmail.com>
2024-05-26 18:02:46 +0500

    Add files via upload

---
+++ return function/main.js
@@ -0,0 +1,8 @@
+function mysum() {
+    var a = 10;
+    var b = 20;
+    var sum = a + b;
+    return sum;
+}
+mysum();
+console.log(mysum());
--- return function/main.ts
+++ return function/main.ts
@@ -1,1 +1,10 @@
+//function/return
+function mysum(){
+    let a=10
+    let b=20
+    let sum= a+b
 
+    return sum
+}
+mysum()
+console.log(mysum())
