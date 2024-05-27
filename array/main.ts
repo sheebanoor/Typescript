6469d34baafc313ade1b07d152cbd9a181b91645
sheeba noor <sheebanoor130@gmail.com>
2024-05-27 16:05:34 +0500

    Add files via upload

--- array/main.ts
+++ array/main.ts
@@ -1,1 +1,32 @@
+// //array
+// let fruits=["banana", "orange","apple"]
+// console.log(fruits)
+// console.log(fruits[1])
 
+// //pop
+// let fruit=["banana", "orange","apple"]
+// console.log(fruit.pop())
+// console.log(fruit)
+
+//push
+// let abc=["ali","ahmed","khan"]
+// console.log(abc.push("zahid"))
+// console.log(abc)
+// //shift
+// let fruits=["apple","banana","orange"]
+// console.log(fruits.shift())
+// console.log(fruits)
+
+//unshift
+// let fruit=["apple","banana","orange"]
+// console.log(fruit.unshift("pineapple"))
+// console.log(fruit)
+//
+//slice 
+// let numbers=[1,2,3,4,5,6]
+// console.log(numbers.slice(2,5))
+
+//splice
+let naming=["sarah","fatima","sheeba","adeel","salar"]
+console.log(naming.splice(1,0,"ali","mansoor"))
+console.log(naming)
