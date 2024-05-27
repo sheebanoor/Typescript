7798a443aa7af07a7e82828759defc89ee408172
sheeba noor <sheebanoor130@gmail.com>
2024-05-27 10:50:49 +0500

    Add files via upload

---
+++ return&arrowfunc/main.js
@@ -0,0 +1,19 @@
+//function/return
+function mysum() {
+    var a = 10;
+    var b = 20;
+    var sum = a + b;
+    return sum;
+}
+mysum();
+console.log(mysum());
+//arrow function
+var arrowfunc = function () {
+    console.log("bilal");
+};
+arrowfunc();
+var abc = function () {
+    var abc = "ali";
+    return abc;
+};
+console.log(abc());
--- return&arrowfunc/main.ts
+++ return&arrowfunc/main.ts
@@ -1,1 +1,23 @@
+//function/return
+function mysum(){
+    let a=10
+    let b=20
+    let sum= a+b
 
+    return sum
+}
+mysum()
+console.log(mysum())
+
+
+//arrow function
+let arrowfunc=()=>{
+    console.log("bilal")
+}
+arrowfunc()
+
+let abc=():string =>{
+    let abc:string="ali"
+    return abc
+}
+console.log(abc())
