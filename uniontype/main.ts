cca9ee326d88fffa9625830651ce0431f852890d
sheeba noor <sheebanoor130@gmail.com>
2024-05-30 17:30:52 +0500

    Add files via upload

--- uniontype/main.ts
+++ uniontype/main.ts
@@ -1,1 +1,18 @@
+//union type
 
+//basic union type
+let myName:string| number |boolean=("ali")
+myName =244
+myName ="ali"
+myName =true
+
+//union type in function
+function myfun (name:string | number |boolean ){
+    console.log(name)
+}
+myfun("ali")
+
+//union type in Array
+let myarray=(name :string|boolean |number){
+    console.log("orange","apple",)
+}
